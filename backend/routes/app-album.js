baf475a164f744f4b9345bc60137013385d94815
Borloo <supermaxime753@gmail.com>
2024-01-11 22:41:52 +0100

    post album

--- backend/routes/app-album.js
+++ backend/routes/app-album.js
@@ -4,7 +4,7 @@ const {initModels} = require('../models/init-models');
 const {sequelize} = require("./../models");
 const ModelService = require('./../services/ModelService');
 
-const {Album} = initModels(sequelize);
+const {Album, Artist} = initModels(sequelize);
 const modelService = new ModelService();
 
 router.get('/', async (req, res) => {
@@ -47,6 +47,41 @@ router.get('/:id/songs', async (req, res) => {
     }
 });
 
-
+router.post('/', async (req, res) => {
+    try {
+        const { title, cover_image, release_date, artist_id } = req.body;
+        if (!title){
+            res.status(400).json({ error: 'Title required' });
+            return;
+        }
+        if (!cover_image){
+            res.status(400).json({ error: 'Cover image required' });
+            return;
+        }
+        if (!release_date){
+            res.status(400).json({ error: 'Release date required' });
+            return;
+        }
+        if (!artist_id){
+            res.status(400).json({ error: 'Artist id required' });
+            return;
+        }
+        const artist = await Artist.findByPk(artist_id);
+        if (!artist){
+            res.status(409).json({error: 'Unknow artist id: ' + artist_id});
+            return;
+        }
+        const album = await Album.create({
+            title,
+            cover_image,
+            release_date,
+            artist_id
+        });
+        res.status(201).json(album);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
 module.exports = router;
