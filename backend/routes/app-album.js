e0e0c1f90cfdb2974dae34a24e2926908639175a
Borloo <supermaxime753@gmail.com>
2024-01-11 21:25:57 +0100

    get album by id + model complete for query response

--- backend/routes/app-album.js
+++ backend/routes/app-album.js
@@ -1,18 +1,33 @@
 const express = require('express');
 const router = express.Router();
-const { initModels } = require('../models/init-models');
+const {initModels} = require('../models/init-models');
 const {sequelize} = require("./../models");
 
-const { Album, Artist } = initModels(sequelize);
+const {Album, Artist, Song, Gender} = initModels(sequelize);
 
 const model_album = {
     attributes: {
         exclude: ['artist_id']
     },
-    include: [{
-        model: Artist,
-        as: 'artist'
-    }]
+    include: [
+        {
+            model: Song,
+            as: 'songs',
+            attributes: {
+                exclude: ['artist_id', 'album_id', 'gender_id']
+            },
+            include: [
+                {
+                    model: Gender,
+                    as: 'gender'
+                }
+            ]
+        },
+        {
+            model: Artist,
+            as: 'artist'
+        },
+    ]
 }
 
 router.get('/', async (req, res) => {
@@ -21,7 +36,22 @@ router.get('/', async (req, res) => {
         res.status(201).json(albums);
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        res.status(500).json({error: 'Internal Server Error'});
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    const album_id = req.params.id;
+    try {
+        const album = await Album.findByPk(album_id, model_album);
+        if (!album) {
+            res.status(409).json({error: 'Unknow album id: ' + album_id});
+            return;
+        }
+        res.status(201).json(album);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({error: 'Internal Server Error'});
     }
 });
 
