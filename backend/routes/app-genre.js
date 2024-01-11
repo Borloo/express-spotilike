d3dcf500837291ef8fa9024525ab9b0543a0a426
Borloo <supermaxime753@gmail.com>
2024-01-11 13:14:36 +0100

    put by id

--- backend/routes/app-genre.js
+++ backend/routes/app-genre.js
@@ -15,4 +15,20 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.put('/:id', async (req, res) => {
+    const genre_id = req.params.id;
+    try {
+        const genre = await Genre.findByPk(genre_id);
+        if (!genre){
+            res.status(409).json({ error: 'Unknow genre id: ' + genre_id });
+            return;
+        }
+        await genre.update(req.body);
+        res.status(201).json(genre);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
 module.exports = router;
