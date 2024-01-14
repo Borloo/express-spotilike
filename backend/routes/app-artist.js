508f73f7757b418d618002d49bd720219f9eaf8b
Borloo <supermaxime753@gmail.com>
2024-01-14 15:38:40 +0100

    cascade delete artist by id

--- backend/routes/app-artist.js
+++ backend/routes/app-artist.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { initModels } = require('../models/init-models');
+const {initModels} = require('../models/init-models');
 const {sequelize} = require("./../models");
 
-const { Artist, Song, Song_Artist } = initModels(sequelize);
+const {Artist, Song, Album} = initModels(sequelize);
 
 router.get('/', async (req, res) => {
     try {
@@ -11,7 +11,7 @@ router.get('/', async (req, res) => {
         res.status(201).json(artists);
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        res.status(500).json({error: 'Internal Server Error'});
     }
 });
 
@@ -25,13 +25,13 @@ router.get('/:id/songs', async (req, res) => {
             }]
         });
         if (!artist) {
-            res.status(409).json({ error: 'Unknow artist id: ' + artist_id });
+            res.status(409).json({error: 'Unknow artist id: ' + artist_id});
             return;
         }
         res.status(201).json(artist.Songs);
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        res.status(500).json({error: 'Internal Server Error'});
     }
 });
 
@@ -39,31 +39,41 @@ router.put('/:id', async (req, res) => {
     const artist_id = req.params.id;
     try {
         const artist = await Artist.findByPk(artist_id);
-        if (!artist){
-            res.status(409).json({ error: 'Unknow artist id: ' + artist });
+        if (!artist) {
+            res.status(409).json({error: 'Unknow artist id: ' + artist});
             return;
         }
         await artist.update(req.body);
         res.status(201).json(artist);
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        res.status(500).json({error: 'Internal Server Error'});
     }
 });
 
 router.delete("/:id", async (req, res) => {
     const artist_id = req.params.id;
-    try{
+    try {
         const artist = await Artist.findByPk(artist_id);
-        if (!artist){
-            res.status(409).json({ error: 'Unknow artist id: ' + artist_id });
+        if (!artist) {
+            res.status(409).json({error: 'Unknow artist id: ' + artist_id});
             return;
         }
+        await Song.destroy({
+            where: {
+                artist_id: artist_id
+            }
+        });
+        await Album.destroy({
+            where: {
+                artist_id: artist_id
+            }
+        });
         await artist.destroy();
-        res.status(200).json({message: 'Artist id: ' + artist_id + ' deleted' });
-    } catch (error){
+        res.status(200).json({message: 'Artist id: ' + artist_id + ' deleted'});
+    } catch (error) {
         console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        res.status(500).json({error: 'Internal Server Error'});
     }
 });
 
