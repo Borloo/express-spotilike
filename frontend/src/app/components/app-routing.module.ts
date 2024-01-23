2444ef85427a35cbf02142194aba68509a4cdbf4
Borloo <supermaxime753@gmail.com>
2024-01-23 13:34:18 +0100

    [FRONT] Home route + Albums and artists buttons [INTE] Toggle templates

--- frontend/src/app/components/app-routing.module.ts
+++ frontend/src/app/components/app-routing.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'artist',
-    loadChildren: () => import('./artists-list/artist-routing.module').then(a => a.ArtistsRoutingModule),
-  },
-  {path: '', redirectTo: 'artist/list', pathMatch: 'full'},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
   {
     path: 'account',
     loadChildren: () => import('./account/account-routing.module').then(a => a.AccountRoutingModule),
   },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home-routing.module').then(a => a.HomeRoutingModule),
+  }
  ];
 
 @NgModule({
